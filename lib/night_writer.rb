08f728308871b56a165c1d7fd803deceb1d597d0
NatBee <nataliesbarron@gmail.com>
2017-02-04 20:54:00 -0700

    Night writer class pseudocode

---
+++ lib/night_writer.rb
@@ -0,0 +1,23 @@
+class NightWriter
+#instance variable called count
+
+#2 parameters names of input and output files
+#open method
+  #take the input file using file open instance
+  #store the contents of the file to a variable
+
+#count characters file method?
+
+#write method
+  #a string will be passed into it and that string will be written to the file specified in the instantiation
+
+#print method
+
+
+
+
+
+
+
+
+end
