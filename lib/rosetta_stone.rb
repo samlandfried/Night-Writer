6e8e8b8ad253094e80885c11865d249cf630d14b
NatBee <nataliesbarron@gmail.com>
2017-02-04 20:32:52 -0700

    Pseudocode RosettaStone

---
+++ lib/rosetta_stone.rb
@@ -0,0 +1,24 @@
+class RosettaStone
+
+  # Create 3 arrays as instance variables
+    # English alphabet =  " !',-.?abcdefghijklmnopqrstuvwxyz".split('')
+    braille = File.open("lib/braille.txt", "r").read.split("\n")
+    # Braille alphabet = braille
+    # Modifiers = define from somewhere
+
+  # Create four symbols :l, :r, :f, :e
+
+  # Recreate Braille alphabet with those symbols
+  # Make an empty array braille_alphabet
+  # Loop 33 times
+    # create subarr
+    # Loop 3 times
+      # Look at a pair of dots and assign them a symbol
+      # Shovel symbol into array
+    # Shovel subarr into braille_alphabet
+
+  # Push them to an array
+
+end
+
+rs = RosettaStone.new
