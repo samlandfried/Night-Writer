cd8fd813f7f6b38370f552ce19347681bd2c07eb
NatBee <nataliesbarron@gmail.com>
2017-02-04 17:48:47 -0700

    tests have been named

--- test/rosetta_stone_test.rb
+++ test/rosetta_stone_test.rb
@@ -3,34 +3,120 @@ require 'minitest/autorun'
 require 'minitest/emoji'
 # require_relative '../lib/rosetta_stone'
 
-class RosettaStoneTest < Minitest::Test
+class NightWriterTest < Minitest::Test
 
-  def test_it_has_3_arrays
+  describe "rosetta stone" do
+
+
+    def test_it_has_3_arrays
+
+    end
+
+    def test_braille_sub_arrays_contain_symbols
+
+    end
+
+    def test_braille_array_contains_arrays
+
+    end
+
+    def test_english_array_contains_strings
+    end
+
+    def test_it_creates_capitals
+
+    end
+
+    def test_it_matches_characters
+
+    end
+
+    def test_braille_array_has_33_elements
+
+    end
 
   end
 
-  def test_braille_sub_arrays_contain_symbols
+  describe "file IO" do
+
+    def test_it_produces_a_string
+
+    end
+
+    def test_it_makes_a_new_file
+
+    end
+
+    def test_it_includes_a_file_name
+    end
+
 
   end
 
-  def test_braille_array_contains_arrays
+  describe "English to Braille translation" do
+
+    def test_it_contains_1_row
+    end
+
+    def test_it_outputs_braille
+    end
+
+    def test_it_translates_characters
+    end
+
+    def test_it_adds_shift_character
+    end
+
+    def test_it_finds_errors
+    end
+
+    def it_counts_characters
+    end
 
   end
 
-  def test_english_array_contains_strings
+  describe "Braille to English translation" do
+
+    def test_it_contains_3_row
+    end
+
+    def test_it_outputs_english
+    end
+
+    def test_it_translates_characters
+    end
+
+    def test_it_capitalizes
+    end
+
+    def test_it_finds_errors
+    end
+
+    def it_counts_characters
+    end
+
   end
 
-  def test_it_creates_capitals
+  describe "command line" do
+
+  def test_it_puts
+  end
+
+  def test_it_does_something_to_a_file_name
+  end
+
+  def test_it_includes_a_character_count
+  end
+
 
   end
 
-  def test_it_matches_characters
+end
 
-  end
 
-  def test_braille_array_has_33_elements
 
-  end
+
+end
 
 
 end
