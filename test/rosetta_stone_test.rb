45b62ffe3abbacb00cae2b7e136b6e4d30f54b5d
NatBee <nataliesbarron@gmail.com>
2017-02-04 17:20:31 -0700

    rosetta stone test setup

---
+++ test/rosetta_stone_test.rb
@@ -0,0 +1,36 @@
+gem 'minitest', '~> 5.0'
+require 'minitest/autorun'
+require 'minitest/emoji'
+# require_relative '../lib/rosetta_stone'
+
+class RosettaStoneTest < Minitest::Test
+
+  def test_it_has_3_arrays
+
+  end
+
+  def test_braille_sub_arrays_contain_symbols
+
+  end
+
+  def test_braille_array_contains_arrays
+
+  end
+
+  def test_english_array_contains_strings
+  end
+
+  def test_it_creates_capitals
+
+  end
+
+  def test_it_matches_characters
+
+  end
+
+  def test_braille_array_has_33_elements
+
+  end
+
+
+end
