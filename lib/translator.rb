7bba94957f0e26c7df6eecee9eb03a7b29246c0e
Sam Landfried <samlandfried@gmail.com>
2017-02-05 17:12:46 -0700

    Pseudocode .translate_braille_to_english

---
+++ lib/translator.rb
@@ -0,0 +1,16 @@
+class Translator
+
+  def initialize untranslated
+    @untranslated = untranslated # array of braille characters
+    @translated = ""
+  end
+
+  def translate_braille_to_english
+    # An empty array to hold the result
+    # Iterate over untranslated
+      # Use rosetta_stone to find equivalent value
+      # Shovel new value into storage array
+    # Convert storage into a string
+    # Return string
+  end
+end
