002f9b2933195945b96b213080986f683f46bc78
NatBee <nataliesbarron@gmail.com>
2017-02-05 17:38:39 -0700

    pseudocode symbol braille to string braille

--- lib/rosetta_stone.rb
+++ lib/rosetta_stone.rb
@@ -16,9 +16,20 @@ class RosettaStone
       # Look at a pair of dots and assign them a symbol
       # Shovel symbol into array
     # Shovel subarr into braille_alphabet
+    # Push them to an array
 
+  #convert symbol braille to string braille
+    #input is an array of symbol braille elements
+    #row 1 string
+    #row 2 string
+    #row 3 string
+    #iterate over input
+      #iterate over symbol braille elements
+        #convert the symbols in row i using rosetta_stone
+        #concat converted symbol to row i string
+    #push rows into an array
+    #return array
 
-  # Push them to an array
 
 end
 
